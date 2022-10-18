5daa28000344c96cbe40b773740fe45874ea12e2
Santhana16 <rashithcr7@gmail.com>
2022-10-18 22:59:33 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssesmentComponent } from './Component/assesment/assesment.component';
 import { LoginComponent } from './Component/login/login.component';
 const routes: Routes = [
   { path: "Login", component: LoginComponent },
+  { path: "todo", component: LoginComponent },
+  {path: "assesment", component: AssesmentComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Component/login/login.component';
-
+import { AssesmentComponent } from './Component/assesment/assesment.component';
+import { RightsectionComponent } from './Component/rightsection/rightsection.component';
+import { RnavbarComponent } from './Component/rnavbar/rnavbar.component';
+import { RightmidComponent } from './Component/rightmid/rightmid.component';
+import { RightbottomComponent } from './Component/rightbottom/rightbottom.component';
+import { ScrollerComponent } from './Component/scroller/scroller.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AssesmentComponent,
+    RightsectionComponent,
+    RnavbarComponent,
+    RightmidComponent,
+    RightbottomComponent,
+    ScrollerComponent
   ],
   imports: [
     BrowserModule,
